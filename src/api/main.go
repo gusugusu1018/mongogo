63a552f5366e4b6afa0014be6571c892d72a2e87
gusugusu1018 <gusugusu1018@gmail.com>
2020-01-16 14:44:28 +0900

    udpate

--- src/api/main.go
+++ src/api/main.go
@@ -1,21 +1,35 @@
 package main
+
 import (
-    "context"
-    "fmt"
-    "log"
-    "go.mongodb.org/mongo-driver/mongo"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
-    // Rest of the code will go here
-    client, err := mongo.Connect(context.TODO(), "mongodb://localhost:27017")
-    if err != nil {
-        log.Fatal(err)
-    }
-    // Check the connection
-    err = client.Ping(context.TODO(), nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("Connected to MongoDB!")
+	fmt.Println("$MONGODB_URL:", os.Getenv("MONGODB_URL"))
+	// create client
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// connect
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Check the connection
+	ctx, _ = context.WithTimeout(context.Background(), 2*time.Second)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
 }
